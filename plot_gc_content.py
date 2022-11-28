32cef4c1481efe33d4cf50e2e4a5b3d7e5318dda
georgiyponomarev <georgiy.ponomarev@quantori.academy>
2022-11-28 21:17:18 +0300

    removed separate plotting function

--- plot_gc_content.py
+++ plot_gc_content.py
@@ -6,65 +6,36 @@ import random
 IMAGES_DIR = "./gc-content_ratios"
 
 
-def plot_gc_content_ratio(genome_position: np.array,
-                  gc_content: np.array,
-                  dataset: str = "random DNA sequence",
-                  outfile: str = IMAGES_DIR + "/gc-content.png",
-                  figsize: tuple = (8, 6),
-                  fontsize: int = 14,
-                  linewidth: int = 3,
-                  linecolor: str = "indigo"
-                  ):
+def gc_content_ratio(
+    genomic_data: str, 
+    step: int = 100, 
+    dataset_name: str = "random DNA sequence",
+    outfile_name: str = IMAGES_DIR + "/gc-content.png", 
+    figsize: tuple = (8, 6),
+    fontsize: int = 14,
+    linewidth: int = 3,
+    linecolor: str = "indigo"
+    ):
 
-    """
-       The function for plotting the GC-content ratio
-       in a DNA sequence. 
-       The resulting graph is saved to .png file
-
-       In the graph:
-           * x-axis: Genome position -- position of first base
-                     in a bin
-           * y-axis: GC-content ratio in %
-
-      Input parameters:
-           * genome_position and gc_content -- two numpy arrays, 
-             used for creating the graph -- x and y(x), respectively
-           * other parameters are intended to make the graph 
-             prettier and more descriptive 
-    """
-    
-    plt.figure(figsize = figsize)
-    plt.plot(genome_position, 
-             gc_content, 
-             color = linecolor, 
-             linewidth = linewidth)
-    plt.title(f"Plot of the CG-content distribution in a {dataset}", fontsize = fontsize)
-    plt.xlabel("Genome position", fontsize = fontsize)
-    plt.ylabel("GC-content, %", fontsize = fontsize)
-    plt.xticks(fontsize = fontsize)
-    plt.yticks(fontsize = fontsize)
-    plt.savefig(outfile)
-    plt.close()
-
-
-def gc_content_ratio(genomic_data: str, 
-                     step: int = 100, 
-                     dataset_name: str = "random DNA sequence",
-                     outfile_name: str = IMAGES_DIR + "/gc-content.png"
-                     ):
     """
        A function that 
        (1) calculates CG-content ratio for each of
        the 'step' segments of the molecule, which DNA sequence
        is given in the 'genomic_data' parameter and
-       (2) plots the GC ratio and saves the graph to image file,
-       using external function 'plot_gc_content_ration'
+       (2) plots the GC ratio and saves the graph to image file
+
+       In the plot:
+           * x-axis: Genome position -- position of first base
+                     in a bin
+           * y-axis: GC-content ratio in % 
 
        Input parameters:
           * genomic_data (string) -- a DNA sequence
           * step (int) -- denotes a width of a subsequence within
                           which the GC-content ratio is calculated.
                           By default bin width = 100 characters.
+          * other parameters are intended to make the graph 
+            prettier and more descriptive
     """
 
     molecule_length = len(genomic_data)
@@ -81,10 +52,18 @@ def gc_content_ratio(genomic_data: str,
             genome_position[index] = start_base
         index += 1
 
-    plot_gc_content_ratio(genome_position, 
-                          gc_content, 
-                          dataset = dataset_name,
-                          outfile = outfile_name)
+    plt.figure(figsize = figsize)
+    plt.plot(genome_position,
+             gc_content,
+             color = linecolor,
+             linewidth = linewidth)
+    plt.title(f"Plot of the CG-content distribution in a {dataset_name}", fontsize = fontsize)
+    plt.xlabel("Genome position", fontsize = fontsize)
+    plt.ylabel("GC-content, %", fontsize = fontsize)
+    plt.xticks(fontsize = fontsize)
+    plt.yticks(fontsize = fontsize)
+    plt.savefig(outfile_name)
+    plt.close()
 
 
 # *** Some tests ***
