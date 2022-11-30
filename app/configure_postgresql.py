e18b9bac35acb0e2f45e665b001397b8d66d0e41
georgiyponomarev <georgiy.ponomarev@quantori.academy>
2022-11-30 19:02:38 +0300

    created folder app and put all the code stuff there

---
+++ app/configure_postgresql.py
@@ -0,0 +1,13 @@
+import os
+
+# Install postgresql server: uncomment the following lines to install
+# os.system("sudo apt-get -y -qq update")
+# os.system("sudo apt-get -y -qq install postgresql")
+# os.system("sudo service postgresql start")
+
+# Setup a password `postgres` for username `postgres`
+# os.system("sudo -u postgres psql -U postgres -c \"ALTER USER postgres PASSWORD 'postgres';\"")
+
+# Setup a database with name `tfio_demo` to be used
+os.system("sudo -u postgres psql -U postgres -c 'DROP DATABASE IF EXISTS geneticdb;'")
+os.system("sudo -u postgres psql -U postgres -c 'CREATE DATABASE geneticdb;'")
